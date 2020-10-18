0822d6eced2a7da230e285e5fa36de4161ab29d0
MKamill <mirgayazow2014@yandex.ru>
2020-10-18 16:46:42 +0500

    finaly js ----> ts

--- src/contacts.tsx
+++ src/contacts.tsx
@@ -5,7 +5,7 @@ function Contacts() {
   return (
     <div className='Contacts'>
       <h5>Phone number: 89876055272</h5>
-      <h5>E-mail: mirgayazow2014@yandex.ru</h5>
+      <h5> E-mail: mirgayazow2014@yandex.ru</h5>
     </div>
   );
 }
